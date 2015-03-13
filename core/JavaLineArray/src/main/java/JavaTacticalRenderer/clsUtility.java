79f55402e3da68055aca3c5a5f0225c8d7c79e8e
Michael Deutch <mdeutch3@comcast.net>
2015-03-13 12:00:21 -0400

    Increased the interpolation interval for anititank ditch reinforced with mines from 25 to 50. 25 pixels was not a long enough interval to allow for placement of the glyph which includes the spike and the mine, so that zooming out causes the glyphs to disappear.

--- core/JavaLineArray/src/main/java/JavaTacticalRenderer/clsUtility.java
+++ core/JavaLineArray/src/main/java/JavaTacticalRenderer/clsUtility.java
@@ -3127,8 +3127,10 @@ public final class clsUtility {
             {
                 case TacticalLines.ATDITCH:
                 case TacticalLines.ATDITCHC:
-                case TacticalLines.ATDITCHM:
                     glyphSize=25;
+                    break;
+                case TacticalLines.ATDITCHM:
+                    glyphSize=50;
                     break;
                 case TacticalLines.DMAF:
                     glyphSize=20;
