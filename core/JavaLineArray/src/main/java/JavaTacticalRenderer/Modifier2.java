030854a396a45a8c76657a805acc388a52913f82
Michael Deutch <mdeutch3@comcast.net>
2015-04-10 18:40:49 -0400

    Rev D modifier fixes.

--- core/JavaLineArray/src/main/java/JavaTacticalRenderer/Modifier2.java
+++ core/JavaLineArray/src/main/java/JavaTacticalRenderer/Modifier2.java
@@ -5542,7 +5542,7 @@ public class Modifier2 {
                 case 140102:
                 case 150101:
                 case 150102:
-                case 151801:
+                //case 151801:
                 case 151900:
                 case 152000:
                 case 152100:
@@ -5557,7 +5557,7 @@ public class Modifier2 {
                     break;
                 case 150103:
                 case 150104:
-                case 151802:
+                //case 151802:
                     areasWithENY(tg, g2d);
                     break;               
                 case 150501:
