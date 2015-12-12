d94100a6009353e43b67de8587a23e1aa1a42e0a
Michael Deutch <mdeutch3@comcast.net>
2015-12-11 19:02:43 -0500

    Removed line interpolation for Boundary. Please test with the performance enhancement in the Modifier2 class before we resort to using line interpolation for boundary.

--- core/JavaRendererServer/src/main/java/RenderMultipoints/clsRenderer.java
+++ core/JavaRendererServer/src/main/java/RenderMultipoints/clsRenderer.java
@@ -1445,18 +1445,18 @@ public final class clsRenderer {
             tg.Pixels = savePixels;
             
             //diagnostic
-            HashMap<Integer, Color> hmap = JavaTacticalRenderer.clsUtility.getMSRSegmentColors(tg);
-            if(hmap==null || hmap.isEmpty())
-            {
-                if(linetype==TacticalLines.BOUNDARY)
-                {
-                    //use flot for the interpolation interval, this is subject to change
-                    tg.set_LineType(TacticalLines.FLOT);
-                    JavaTacticalRenderer.clsUtility.InterpolatePixels(tg);
-                    //reset the linetype
-                    tg.set_LineType(TacticalLines.BOUNDARY);
-                }
-            }
+//            HashMap<Integer, Color> hmap = JavaTacticalRenderer.clsUtility.getMSRSegmentColors(tg);
+//            if(hmap==null || hmap.isEmpty())
+//            {
+//                if(linetype==TacticalLines.BOUNDARY)
+//                {
+//                    //use flot for the interpolation interval, this is subject to change
+//                    tg.set_LineType(TacticalLines.FLOT);
+//                    JavaTacticalRenderer.clsUtility.InterpolatePixels(tg);
+//                    //reset the linetype
+//                    tg.set_LineType(TacticalLines.BOUNDARY);
+//                }
+//            }
             //end section
 
             ArrayList rangeFanFillShapes = null;
