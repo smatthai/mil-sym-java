ad60118dbd4e48e9c9048480a22695ef2e31254a
Michael Deutch <mdeutch3@comcast.net>
2017-02-08 11:58:46 -0500

    Corrected logic error induced by the previous fix to intersectsClipArea.

--- core/JavaRendererServer/src/main/java/RenderMultipoints/clsRenderer.java
+++ core/JavaRendererServer/src/main/java/RenderMultipoints/clsRenderer.java
@@ -1264,7 +1264,7 @@ public final class clsRenderer {
             boolean coordSpanIDL = false;
             if(maxx==180 || minx==-180)
                 coordSpanIDL=true;
-            else if(maxx-minx>=180)
+            if(maxx-minx>=180)
             {
                 coordSpanIDL=true;
                 coordsLeft=maxx;
